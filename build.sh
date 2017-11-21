1c14ac7d33c2192a21e9e99bb054fbcbe95f1437
Fernando del Rio <fernandomdr@gmail.com>
2017-11-21 20:24:07 -0200

    Adding build.sh

---
+++ build.sh
@@ -0,0 +1,15 @@
+mkdir -p bin
+swiftc \
+SwiftColorGen/main.swift \
+SwiftColorGen/Libs/AEXML/Document.swift \
+SwiftColorGen/Libs/AEXML/Element.swift \
+SwiftColorGen/Libs/AEXML/Error.swift \
+SwiftColorGen/Libs/AEXML/Options.swift \
+SwiftColorGen/Libs/AEXML/Parser.swift \
+SwiftColorGen/Extensions/AEXMLElement.swift \
+SwiftColorGen/Model/ColorData.swift \
+SwiftColorGen/Model/WebColors.swift \
+SwiftColorGen/Utils/CLIManager.swift \
+SwiftColorGen/Utils/ColorManager.swift \
+SwiftColorGen/Utils/PathManager.swift \
+SwiftColorGen/Utils/StoryboardManager.swift -o swiftcg
