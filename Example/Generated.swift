341491b2ea9610618e5f70da51a5a6ae26d61418
Fernando del Rio <fernandomdr@gmail.com>
2017-11-21 19:54:30 -0200

    Updating resources

--- Example/Generated.swift
+++ Example/Generated.swift
@@ -1,1 +1,44 @@
+// Don't change. Auto generated file. SwiftColorGen
 
+extension UIColor {
+	class func genWhite() -> UIColor {
+		return UIColor(named: "White") ?? UIColor.white
+	}
+
+	class func genDarkOrchid() -> UIColor {
+		return UIColor(named: "DarkOrchid") ?? UIColor.white
+	}
+
+	class func genTeal() -> UIColor {
+		return UIColor(named: "Teal") ?? UIColor.white
+	}
+
+	class func genChartreuse() -> UIColor {
+		return UIColor(named: "Chartreuse") ?? UIColor.white
+	}
+
+	class func genRed() -> UIColor {
+		return UIColor(named: "Red") ?? UIColor.white
+	}
+
+	class func genBlack() -> UIColor {
+		return UIColor(named: "Black") ?? UIColor.white
+	}
+
+	class func genDarkOrange() -> UIColor {
+		return UIColor(named: "DarkOrange") ?? UIColor.white
+	}
+
+	class func genYellow() -> UIColor {
+		return UIColor(named: "Yellow") ?? UIColor.white
+	}
+
+	class func genDeepPink() -> UIColor {
+		return UIColor(named: "DeepPink") ?? UIColor.white
+	}
+
+	class func genBlack_alpha0() -> UIColor {
+		return UIColor(named: "Black (alpha 0)") ?? UIColor.white
+	}
+
+}
