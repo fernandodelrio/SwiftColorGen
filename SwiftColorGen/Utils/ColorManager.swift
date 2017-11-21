4739a0a79b259433243383b33d5d1d9f08a32eb7
Fernando del Rio <fernandomdr@gmail.com>
2017-11-21 19:25:43 -0200

    Fixing the issue when updating the storyboard

--- SwiftColorGen/Utils/ColorManager.swift
+++ SwiftColorGen/Utils/ColorManager.swift
@@ -19,15 +19,12 @@ struct ColorManager {
                 let green = xml.attributes["green"],
                 let blue = xml.attributes["blue"],
                 let alpha = xml.attributes["alpha"] {
-                
                 let color = ColorData()
                 color.red = Double(red) ?? 0.0
                 color.green = Double(green) ?? 0.0
                 color.blue = Double(blue) ?? 0.0
                 color.alpha = Double(alpha) ?? 0.0
                 colors.insert(color)
-                
-                setColor(xml: xml, name: color.name)
             }
             for child in xml.children {
                 read(xml: child)
@@ -35,6 +32,32 @@ struct ColorManager {
         }
         read(xml: xml)
         return colors
+    }
+    
+    static func updateColors(xml: AEXMLElement, colors: Set<ColorData>) {
+        let generatorData = getColorsForGenerator(colors: colors)
+        func read(xml: AEXMLElement) {
+            if xml.name == "color",
+                let colorSpace = xml.attributes["customColorSpace"],
+                colorSpace == "sRGB",
+                let red = xml.attributes["red"],
+                let green = xml.attributes["green"],
+                let blue = xml.attributes["blue"],
+                let alpha = xml.attributes["alpha"] {
+                let color = ColorData()
+                color.red = Double(red) ?? 0.0
+                color.green = Double(green) ?? 0.0
+                color.blue = Double(blue) ?? 0.0
+                color.alpha = Double(alpha) ?? 0.0
+                if let data = (generatorData.filter { $0.color == color }.first) {
+                    setColor(xml: xml, name: data.assetName)
+                }
+            }
+            for child in xml.children {
+                read(xml: child)
+            }
+        }
+        read(xml: xml)
     }
     
     // Update the storyboard color from a raw value
