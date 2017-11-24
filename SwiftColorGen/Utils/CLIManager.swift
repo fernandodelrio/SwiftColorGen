f3fb2c3e732f96c82f446fca2136a1623acb11c9
Fernando del Rio <fernandomdr@gmail.com>
2017-11-23 22:03:05 -0200

    Improving the command line interface

--- SwiftColorGen/Utils/CLIManager.swift
+++ SwiftColorGen/Utils/CLIManager.swift
@@ -9,35 +9,110 @@ import Foundation
 
 struct CLIManager {
     // Returns the arguments in a valid format
-    static func getArgs(arguments: [String]) -> (baseFolder: String, assetsFolder: String, outputFile: String)? {
-        guard arguments.count == 4 else {
-            return nil
+    static func getArgs() -> (baseFolder: String, assetsFolder: String, outputFile: String) {
+        let cli = CommandLine()
+        
+        let assetsFolderPath = StringOption(shortFlag: "a",
+                                            longFlag: "assetsFolder",
+                                            required: false,
+                                            helpMessage: "Path to the assets folder. Defaults to the first .xcassets found inside the baseFolder")
+        
+        let baseFolderPath = StringOption(shortFlag: "b",
+                                          longFlag: "baseFolder",
+                                          required: false,
+                                          helpMessage: "Path to the folder where the storyboards are located. Defaults to the current working directory")
+        
+        let outputFilePath = StringOption(shortFlag: "o",
+                                          longFlag: "outputFile",
+                                          required: true,
+                                          helpMessage: "Path to the output Swift file")
+        
+        let help = BoolOption(shortFlag: "h",
+                              longFlag: "help",
+                              helpMessage: """
+                                            Usage: ./swiftcg [options]
+                                            -o --outputFile (required):
+                                                Path to the output Swift file
+                                            -b --baseFolder (optional):
+                                                Path to the folder where the storyboards are located. Defaults to the current working directory
+                                            -a --assetsFolder (optional):
+                                                Path to the assets folder. Defaults to the first .xcassets found under the base folder
+                                           """)
+        
+        cli.addOptions([assetsFolderPath, baseFolderPath, outputFilePath, help])
+        
+        do {
+            try cli.parse()
+        } catch {
+            print(help.helpMessage)
+            exit(1)
         }
-        guard let baseFolder = (arguments.filter { $0.hasPrefix("baseFolder=") }.first),
-              let assetsFolder = (arguments.filter { $0.hasPrefix("assetsFolder=") }.first),
-              let outputFile = (arguments.filter { $0.hasPrefix("outputFile=") }.first) else {
-            return nil
+        
+        if help.value {
+            print(help.helpMessage)
+            exit(0)
         }
-        let result = (baseFolder: getValue(argument: baseFolder),
-                      assetsFolder: getValue(argument: assetsFolder),
-                      outputFile: getValue(argument: outputFile))
-        guard PathManager.isAbsolute(path: result.baseFolder),
-              PathManager.isAbsolute(path: result.assetsFolder),
-              PathManager.isAbsolute(path: result.outputFile) else {
-            return nil
+        
+        if cli.unparsedArguments.count > 0 {
+            print(help.helpMessage)
+            exit(1)
         }
-        return result
-    }
-    
-    private static func getValue(argument: String) -> String {
-        let values = argument.split(separator: "=")
-        guard values.count == 2 else {
-            return ""
+        
+        var assetsFolder = assetsFolderPath.value
+        var baseFolder = baseFolderPath.value
+        var outputFile = outputFilePath.value
+        
+        if assetsFolder == "/" || baseFolder == "/" {
+            print("Potentially dangerous. Don't run the tool on the root folder")
+            exit(1)
         }
-        let value = String(argument.split(separator: "=")[1])
-        if value.hasSuffix("/") {
-            return String(value.prefix(value.count-1))
+        
+        if (assetsFolder ?? "").hasPrefix("..") ||
+           (baseFolder ?? "").hasPrefix("..") ||
+           (outputFile ?? "").hasPrefix("..") {
+            print("Parent path syntax not supported. Please inform a valid path")
+            exit(1)
         }
-        return value
+        
+        if let folder = assetsFolder, folder.hasPrefix(".") {
+            assetsFolder = PathManager.replaceCurrentPath(folder)
+        }
+        if let folder = baseFolder, folder.hasPrefix(".") {
+            baseFolder = PathManager.replaceCurrentPath(folder)
+        }
+        if let file = outputFile, file.hasPrefix(".") {
+            outputFile = PathManager.replaceCurrentPath(file)
+        }
+        
+        if assetsFolder == nil {
+            assetsFolder = PathManager.getAssetsFolder()
+            if assetsFolder == nil {
+                print("Assets folder not found. Please specify the path for the .xcassets folder")
+                exit(1)
+            }
+        }
+        
+        if let assetsFolder = assetsFolder, !PathManager.isValidAssetsFolder(path: assetsFolder) {
+            print("Invalid Assets folder. Please specify the path for the .xcassets folder")
+            exit(1)
+        }
+        
+        if baseFolder == nil {
+            baseFolder = FileManager.default.currentDirectoryPath
+        }
+        
+        if let folder = assetsFolder, folder.hasSuffix("/") {
+            assetsFolder = String(folder.prefix(folder.count-1))
+        }
+        if let folder = baseFolder, folder.hasSuffix("/") {
+            baseFolder = String(folder.prefix(folder.count-1))
+        }
+        if let folder = outputFile, folder.hasSuffix("/") {
+            outputFile = String(folder.prefix(folder.count-1))
+        }
+        
+        return (baseFolder: PathManager.convertToAbsolutePath(baseFolder ?? ""),
+                assetsFolder: PathManager.convertToAbsolutePath(assetsFolder ?? ""),
+                outputFile: PathManager.convertToAbsolutePath(outputFile ?? ""))
     }
 }
--- SwiftColorGen/Utils/PathManager.swift
+++ SwiftColorGen/Utils/PathManager.swift
@@ -29,7 +29,51 @@ struct PathManager {
         return true
     }
     
+    static func isValidAssetsFolder(path: String) -> Bool {
+        guard path.hasSuffix(".xcassets") || path.hasSuffix(".xcassets/")  else {
+            return false
+        }
+        guard !path.contains("Packages/") else {
+            return false
+        }
+        guard !path.contains(".build/") else {
+            return false
+        }
+        guard !path.contains("Pods/") else {
+            return false
+        }
+        guard !path.contains("Carthage/") else {
+            return false
+        }
+        return true
+    }
+    
+    static func getAssetsFolder() -> String? {
+        let cwd = FileManager.default.currentDirectoryPath
+        let enumerator = FileManager.default.enumerator(atPath: cwd)
+        while let path = enumerator?.nextObject() as? String {
+            if PathManager.isValidAssetsFolder(path: path) {
+                return path
+            }
+        }
+        return nil
+    }
+    
+    static func convertToAbsolutePath(_ path: String) -> String {
+        if isAbsolute(path: path) {
+            return path
+        }
+        let path = path.hasSuffix("/") ? String(path.prefix(path.count-1)) : path
+        let cwd = FileManager.default.currentDirectoryPath
+        return "\(cwd)/\(path)"
+    }
+    
+    static func replaceCurrentPath(_ path: String) -> String {
+        return path.replacingCharacters(in: path.startIndex...path.startIndex,
+                                        with: FileManager.default.currentDirectoryPath)
+    }
+    
     static func isAbsolute(path: String) -> Bool {
-        return path.hasPrefix("/")
+        return path.hasPrefix("/") || path.hasPrefix(".")
     }
 }
