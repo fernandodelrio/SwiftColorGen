47040e4d95e173d5fdbdbd72e509d40508f7a830
Fernando del Rio <fernandomdr@gmail.com>
2017-12-02 19:33:27 -0200

    Removing generated file

--- Example/Generated.swift
+++ Example/Generated.swift
@@ -1,14 +1,1 @@
-// Don't change. Auto generated file. SwiftColorGen
-import UIKit
 
-extension UIColor {
-	/// Color #8D00FF (alpha 255)
-	class func darkVioletColor() -> UIColor {
-		return UIColor(named: "DarkViolet") ?? .clear
-	}
-
-	/// Color #FFFFFF (alpha 255)
-	class func whiteColor() -> UIColor {
-		return UIColor(named: "White") ?? .clear
-	}
-}
